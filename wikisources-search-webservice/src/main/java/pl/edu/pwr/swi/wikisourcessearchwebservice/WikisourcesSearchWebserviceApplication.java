efd29d35953f2286c5c55acbf0e0c3b7c469a794
Hubert Kościelski <228172@student.pwr.edu.pl>
2020-05-03 20:38:55 +0200

    Initial commit - project preconfiguration

---
+++ wikisources-search-webservice/src/main/java/pl/edu/pwr/swi/wikisourcessearchwebservice/WikisourcesSearchWebserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.swi.wikisourcessearchwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikisourcesSearchWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikisourcesSearchWebserviceApplication.class, args);
+    }
+
+}
---
+++ wikisources-search-webservice/src/test/java/pl/edu/pwr/swi/wikisourcessearchwebservice/WikisourcesSearchWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.swi.wikisourcessearchwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikisourcesSearchWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
