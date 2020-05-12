56738bd4e96bd51997f5be45dbf099b1818896ce
Hubert Kościelski <228172@student.pwr.edu.pl>
2020-05-12 16:36:10 +0200

    Refactor ElasticsearchConfig

--- wikisources-search-webservice/src/main/java/pl/edu/pwr/swi/wikisourcessearchwebservice/config/ElasticsearchConfig.java
+++ wikisources-search-webservice/src/main/java/pl/edu/pwr/swi/wikisourcessearchwebservice/config/ElasticsearchConfig.java
@@ -1,27 +1,24 @@
 package pl.edu.pwr.swi.wikisourcessearchwebservice.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("elasticsearch")
-@Getter
-@Setter
+@EnableConfigurationProperties(ElasticsearchProperties.class)
 public class ElasticsearchConfig {
 
-    private String hostname;
-    private Integer port;
+    @Autowired
+    private ElasticsearchProperties elasticsearchProperties;
 
     @Bean(destroyMethod = "close")
     public RestHighLevelClient esClient() {
         return new RestHighLevelClient(
-                RestClient.builder(new HttpHost(hostname, port))
+                RestClient.builder(new HttpHost(elasticsearchProperties.getHostname(), elasticsearchProperties.getPort()))
         );
     }
 }
---
+++ wikisources-search-webservice/src/main/java/pl/edu/pwr/swi/wikisourcessearchwebservice/config/ElasticsearchProperties.java
@@ -0,0 +1,17 @@
+package pl.edu.pwr.swi.wikisourcessearchwebservice.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties("elasticsearch")
+@ConstructorBinding
+@AllArgsConstructor
+@Getter
+public class ElasticsearchProperties {
+
+    private final String hostname;
+    private final Integer port;
+    private final String index;
+}
