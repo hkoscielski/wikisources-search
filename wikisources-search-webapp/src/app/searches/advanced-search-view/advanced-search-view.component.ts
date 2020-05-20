dbccb2957565d2fc5d51d46b5afcb2d9205c22da
KJakubowski <konrad.mj08@gmail.com>
2020-05-20 20:40:00 +0200

    [Frontend] Added advanced search view.

--- wikisources-search-webapp/src/app/searches/advanced-search-view/advanced-search-view.component.ts
+++ wikisources-search-webapp/src/app/searches/advanced-search-view/advanced-search-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-advanced-search-view',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancedSearchViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onBackClicked() {
+    this.router.navigate(['/main']);
+  }
+
+  onSearchClicked() {
+    this.router.navigate(['/results']);
+  }
 }
